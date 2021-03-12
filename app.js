130df199c3758105642f563592ed832ea83901d3
Alyssa Champion <alyssa.champion2016@gmail.com>
2021-03-12 14:40:42 -0800

    final changes to lab

---
+++ app.js
@@ -0,0 +1,15 @@
+var x = 7
+var y = 5
+const z = "Sunny Days"
+const a = false
+
+if (x === y) {
+console.log("they match")
+}
+else{
+console.log("no luck")
+}
+
+for (let char in z){
+console.log(z[char])
+}
